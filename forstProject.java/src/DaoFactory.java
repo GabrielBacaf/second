3b49bdf423f78e1a8c12765033693cb72937af36
GabrielBacaf <gabryelbaca1@gmail.com>
2024-10-29 20:02:12 -0400

    testeDoTeste

--- forstProject.java/src/DaoFactory.java
+++ forstProject.java/src/DaoFactory.java
@@ -5,6 +5,8 @@ public class DaoFactory{
     public static SellerDao createSellerDao(){
         return new SellerDaoJDBC();
 
+        string teste = null;
+        float od = null;
     }
     }
 }
